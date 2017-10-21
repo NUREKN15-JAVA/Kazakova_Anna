9f2556a551da2ca2b5c0e42a70d122690640d035
Anna Kazakova <anna.kazakova@nure.ua>
2017-10-21 21:19:44 +0300

    Revert "Resources added"
    This reverts commit 7445cf21923f4e2b61e577247b25bba62d37a704.

--- workspace1/usermanagement15-6/src/main/java/ua/nure/kn156/kazakova/User.java
+++ workspace1/usermanagement15-6/src/main/java/ua/nure/kn156/kazakova/User.java
@@ -9,15 +9,6 @@ public class User {
 	private String lastName;
 	private Date date;
 	
-	public User(User user) {
-		id = user.getId();
-		firstName = user.getFirstName();
-		lastName = user.getLastName();
-		date = user.getDate();
-	}
-	public User() {
-		
-	}
 	public Long getId() {
 		return id;
 	}
